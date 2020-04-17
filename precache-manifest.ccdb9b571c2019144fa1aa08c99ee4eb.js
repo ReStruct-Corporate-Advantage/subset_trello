67bb4be39ff147d392c23469f24f3b48b496d7aa
Mohit Nagar <mohit.nagar@publicissapient.com>
2020-04-17 16:30:19 +0530

    Updates

---
+++ precache-manifest.ccdb9b571c2019144fa1aa08c99ee4eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a7bfafb683b4ba164cba6ae65c6d29",
+    "url": "/subset_trello/index.html"
+  },
+  {
+    "revision": "3b862535c24a2c219b37",
+    "url": "/subset_trello/static/css/3.69121389.chunk.css"
+  },
+  {
+    "revision": "1e82fdfda9edd7f1c012",
+    "url": "/subset_trello/static/css/4.a7500e5b.chunk.css"
+  },
+  {
+    "revision": "9b662b73a30fad5fc52f",
+    "url": "/subset_trello/static/css/main.7827b2fb.chunk.css"
+  },
+  {
+    "revision": "5aca1e48ec9cc6ce6202",
+    "url": "/subset_trello/static/js/2.63a4c55e.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/subset_trello/static/js/2.63a4c55e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b862535c24a2c219b37",
+    "url": "/subset_trello/static/js/3.337967cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/subset_trello/static/js/3.337967cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e82fdfda9edd7f1c012",
+    "url": "/subset_trello/static/js/4.420f5a43.chunk.js"
+  },
+  {
+    "revision": "9b662b73a30fad5fc52f",
+    "url": "/subset_trello/static/js/main.2c4a9a2b.chunk.js"
+  },
+  {
+    "revision": "8f1e32a476d671a0ca63",
+    "url": "/subset_trello/static/js/runtime-main.7083d5a5.js"
+  }
+]);
